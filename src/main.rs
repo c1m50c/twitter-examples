9b56a89aaa231847ebef50a98702d863ccf703b4
c1m50c <terminalityms@gmail.com>
2022-02-21 20:14:19 -0600

    Match Guards 🧔🗡

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[derive(Debug, PartialEq)]
+enum Direction {
+    North,
+    East,
+    South,
+    West
+}
+
+fn print_direction(dir: Direction) {
+    match dir {
+        Direction::North => println!("Direction is North!"),
+        d if d == Direction::South => println!("Direction is South!"),
+        d => println!("Direction is not North or South, but {:?}!", d),
+    }
+}
+
 fn main() {
     let option = None.unwrap_or_else(|| {
         3.0 + 0.14
